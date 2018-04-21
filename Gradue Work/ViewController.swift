9666cbf59bc5e923176e5c55cc9dcb3f6d5ccc21
ArmaniK <arman.sm96@gmail.com>
2018-04-21 14:22:54 +0600

    Первая версия для Гошана

---
+++ Gradue Work/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Gradue Work
+//
+//  Created by Арман Каштан on 18.04.2018.
+//  Copyright © 2018 Arman Smagulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func fillDataTableAbout() {
+    }
+}
+
+class AboutTableComponent: UITableView {
+    
+}
+
---
+++ Gradue Work/variable.swift
@@ -0,0 +1,12 @@
+//
+//  variable.swift
+//  Gradue Work
+//
+//  Created by Арман Каштан on 21.04.2018.
+//  Copyright © 2018 Arman Smagulov. All rights reserved.
+//
+
+import Foundation
+
+var buttonAbout: String = "Факультет радиотехники"
+
